1e684efcced5f3a58adae0de82e139c54f0d732a
Sam <kahrom.sam@gmail.com>
2026-05-29 21:54:25 +0300

    Fix base path for dev vs production

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
-  base: '/XPWaster/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/XPWaster/' : '/',
   plugins: [
     react(),
     VitePWA({
